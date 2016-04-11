2f079e01b3b9e79c5c49666cf033a46157db375b
Monica <monica.arnon@gmail.com>
2016-04-11 13:29:01 -0700

    Resized monster images, added game start timer and game end function

--- mylittlemonster/DragImage.swift
+++ mylittlemonster/DragImage.swift
@@ -13,6 +13,13 @@ class DragImage: UIImageView {
     
     var originalPosition: CGPoint!
     
+    //Even though we know this project's target for the dragging and dropping
+    //is a UIImageView, think bigger picture in the instance that we want to 
+    //re-use this class to drag and drop things on something other than a
+    //UIImageView. Since it inherits from UIView (and so does most everything else)
+    //use UIView instead.
+    var dropTarget: UIView?
+    
     override init(frame: CGRect) {
         //Must call the parent initializer which means this one that overrides 
         //has at least the same parameter requirements
@@ -37,20 +44,55 @@ class DragImage: UIImageView {
         //A set of touches is passed in when called so attempt to grab the very 
         //first in the set which will be where the touch began
         if let touch = touches.first {
+            
             //If there is a touches.first we'll enter into this if branch 
             //and we can now assign the position variable to be the 
             //location of that touch within the larger superview
             //(Superview being a reserved word for the entire container that's holding 
             //all our content)
             let position = touch.locationInView(self.superview)
+            
             //Assign the new center as it's being moved
             self.center = CGPointMake(position.x, position.y)
         }
     }
     
     override func touchesEnded(touches: Set<UITouch>, withEvent event: UIEvent?) {
-        //Use self.center as a testing tool
-//        self.center = originalPosition
+        
+        //See initial comment in touchesMoved for touches.first explanation
+        if let touch = touches.first, let target = dropTarget {
+            
+            //Grab the position where the touch ended
+            let position = touch.locationInView(self.superview)
+            
+            //Is where the image was dragged anywhere inside the frame of the
+            //intended target? Intended target is first param, the current position
+            //is the second
+            if CGRectContainsPoint(target.frame, position) {
+                
+                //When the item is confirmed to be dropped on the intended target,
+                //create a notification which the ViewController must listen for.
+                //Must add the observer of the notification when you create one.
+                NSNotificationCenter.defaultCenter().postNotification(NSNotification(name: "onTargetDropped", object: nil))
+            }
+            
+        }
+
+        self.center = originalPosition
+        
+        
     }
     
 }
+
+
+
+
+
+
+
+
+
+
+
+
--- mylittlemonster/ViewController.swift
+++ mylittlemonster/ViewController.swift
@@ -11,13 +11,93 @@ import UIKit
 class ViewController: UIViewController {
     
     @IBOutlet weak var monsterImage:    MonsterImage!
-    //In order for these to actually be draggable, you need to check 
+    @IBOutlet weak var penalty1Image:   UIImageView!
+    @IBOutlet weak var penalty2Image:   UIImageView!
+    @IBOutlet weak var penalty3Image:   UIImageView!
+
+    //In order for these to actually be draggable, you need to check
     //the "User Interaction enabled" checkbox in the attribute inspector
+    
     @IBOutlet weak var foodImage:       DragImage!
     @IBOutlet weak var healthImage:     DragImage!
+    
+    let DIM_ALPHA:  CGFloat     = 0.2
+    let OPAQUE:     CGFloat     = 1.0
+    let MAX_PENALTY             = 3
+    
+    var currentPenalities       = 0
+    var timer: NSTimer!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        //The dropTarget property is in the custom class of DragImage.
+        //It's needed so that we can ask "When the user dragged the food or 
+        //health, did it get released on top of the "dropTarget" which is 
+        //set here to the monsterImage. Awesome!
+        
+        foodImage.dropTarget = monsterImage
+        healthImage.dropTarget = monsterImage
+        
+        //Setup the initial opacity of the penalty images
+        penalty1Image.alpha = DIM_ALPHA
+        penalty2Image.alpha = DIM_ALPHA
+        penalty3Image.alpha = DIM_ALPHA
+        
+        //A notification is created when the user drops health or food onto 
+        //the monster UIView (see Monster.touchesEnded). Here we're creating 
+        //the listener for that event.
+        
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: #selector(ViewController.itemDroppedOnCharacter(_:)), name: "onTargetDropped", object: nil)
+        
+        startTimer()
+    }
+    
+    
+    //Function called by the event listener (observer) for the itemDroppedOnCharacter
+    //event
+    func itemDroppedOnCharacter(notif: AnyObject) {
+        print("ITEM DROPPED ON CHARACTER")
+    }
+    
+    func startTimer() {
+        if timer != nil {
+            //Safety measure if the timer is found to be running
+            timer.invalidate()
+        }
+        
+        //repeats: true means the timer is executed every 3.0 seconds (in our case)
+        //Which means the changeGameState function is called every 3.0 seconds
+        timer = NSTimer.scheduledTimerWithTimeInterval(3.0, target: self, selector: #selector(ViewController.changeGameState), userInfo: nil, repeats: true)
+    }
+    
+    func changeGameState() {
+        currentPenalities += 1
+        
+        if currentPenalities == 1 {
+            penalty1Image.alpha = OPAQUE
+            penalty2Image.alpha = DIM_ALPHA
+            penalty3Image.alpha = DIM_ALPHA
+        } else if currentPenalities == 2 {
+            penalty2Image.alpha = OPAQUE
+            penalty3Image.alpha = DIM_ALPHA
+        } else if currentPenalities >= 3 {
+            penalty3Image.alpha = OPAQUE
+        } else {
+            penalty1Image.alpha = DIM_ALPHA
+            penalty2Image.alpha = DIM_ALPHA
+            penalty3Image.alpha = DIM_ALPHA
+            
+        }
+        
+        if currentPenalities >= MAX_PENALTY {
+            gameEnd()
+        }
+    }
+    
+    func gameEnd() {
+        timer.invalidate()
+        monsterImage.playDeathAnimation()
     }
     
 
